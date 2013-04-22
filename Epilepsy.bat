54fc6793c969913e251ff11fec52ed284ae78464
MBasial <mbasial@yahoo.com>
2013-04-22 12:33:11 -0700

    Create Epilepsy.bat

---
+++ Epilepsy.bat
@@ -0,0 +1,5 @@
+@echo off
+start c:\python32\python DiscoLight.py -200 Ledge 1
+start c:\python32\python DiscoLight.py -220 Ledge 2
+start c:\python32\python DiscoLight.py -180 Ledge 3
+start c:\python32\python DiscoLight.py -200 Ledge 4
